5c9e997f7b3d630f0dd92401547b651a098dcde9
VincentShipsIt <vincent@shipshit.dev>
2026-05-09 16:06:44 +0200

    Simplify CLI issue number helper

--- apps/cli/src/commands/issue-helpers.ts
+++ apps/cli/src/commands/issue-helpers.ts
@@ -1,10 +1,7 @@
 import type { Thread } from '@shipcode/shared';
 import type { CliContext } from '../context';
-import { parseIssueNumberOrExit } from './issue-number';
 
-export function parseIssueNumber(issueNumber: string): number {
-  return parseIssueNumberOrExit(issueNumber);
-}
+export { parseIssueNumberOrExit as parseIssueNumber } from './issue-number';
 
 export function getThreadForIssueOrExit(ctx: CliContext, issueNumber: number): Thread {
   const thread = ctx.threads.getByProjectAndGithubIssue(ctx.project.id, issueNumber);
